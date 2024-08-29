414a26afda2aa7d0b05100b3464153e7c248fdb0
dion <dortegana@hotmail.com>
2024-08-29 09:54:49 -0500

    internal class Program

--- Laboratorio2/Laboratorio2/Program.cs
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Laboratorio2;
+using System;
 
 namespace Laboratorio2
 {
@@ -37,5 +38,26 @@ namespace Laboratorio2
 
 
 }
+
+
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+        Client client = new Client();
+        client.FirstName = "Su nombre";
+        client.LastName = "Su apellido";
+        client.Age = 15;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+    }
+
+}
+
+
+
+
 
 
