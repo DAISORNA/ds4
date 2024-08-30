a3e0c429460ca14731d2ba34e099bbb502412c41
dion <dortegana@hotmail.com>
2024-08-30 04:57:38 -0500

    Actualizando Lab2

--- Laboratorio2/Laboratorio2/Program.cs
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -1,5 +1,4 @@
-﻿using Laboratorio2;
-using System;
+﻿using System;
 
 namespace Laboratorio2
 {
@@ -8,7 +7,7 @@ namespace Laboratorio2
         static void Main(string[] args)
         {
             Client client = new Client();
-            //Ejemplo utilizando las variables de instancia de Clase.
+            // Ejemplo utilizando las variables de instancia de Clase.
             client.FirstName = "Su_Nombre";
             client.LastName = "Su_Apellido";
             client.Age = 15;
@@ -16,12 +15,11 @@ namespace Laboratorio2
 
             Console.WriteLine(client.GetFullName());
         }
-
     }
 
     public class Client
     {
-        //Declarando variables de instancia en clase.
+        // Declarando variables de instancia en clase.
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -29,37 +27,14 @@ namespace Laboratorio2
 
         public string GetFullName()
         {
-            //Utilizando variables de instancia dentro de metodos de la clase.
+            // Utilizando variables de instancia dentro de métodos de la clase.
             return FirstName + " " + LastName;
-
         }
-
     }
 
-
-}
-
-
-internal class Program
-
-{
-    private static void Main(string[] args)
+    public class MyClass
     {
-        Client client = new Client();
-        client.FirstName = "Su nombre";
-        client.LastName = "Su apellido";
-        client.Age = 15;
-        client.Id = 1;
-
-        Console.WriteLine(client.GetFullName());
+        // Declarando variable estática
+        public static int Valor;
     }
-
 }
-
-public class MyClass
-{
-    //Declarando variable estatica
-    public static int Valor;
-}
-
-
--- Laboratorio21/Program.cs
+++ Laboratorio21/Program.cs
@@ -4,33 +4,45 @@ namespace Laboratorio21
 {
     public class Program
     {
-        public static void main()
+        public static void Main()
         {
-            //Asignando valor a variable estatica
+            // Asignando valor a variable estática
             MyClass.Valor = 1;
             Console.WriteLine(MyClass.Valor);
-        }
 
+            // Declaramos variables con un valor de 28
+            var valor1 = 28;
+            var valor2 = valor1; // Asignamos el valor de 'valor1' a 'valor2'
+            valor2 = 30;
+
+            // Imprimiendo en consola variables
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+
+            MyClass object1 = new MyClass(); // Creando nueva instancia
+            object1.Nombre = "Yeison";
+            object1.Edad = 28;
+            MyClass object2 = object1; // Asignando una variable a otra
+
+            // Este cambio en la propiedad afecta tanto a object1 como a object2.
+            object2.Nombre = "Jose";
+
+            // Al imprimir en consola vemos que ambas referencias imprimen el mismo valor 'Jose'
+            Console.WriteLine(object2.Nombre);
+            Console.WriteLine(object1.Nombre);
+
+            char caracter = 'A';
+            string cadena = "Cadena de caracteres";
+        }
     }
+
     public class MyClass
     {
-        //Declarando variable estatica
+        // Declarando variable estática
         public static int Valor;
+
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
     }
-
 }
-
-
-int nombreVariable = 1000;
-
-int valor1 = 28; //Declaramos variables con un valor de 28
-
-valor2 = valor1; //Asignamos el valor de variable1 a valor2
-valor2 = 30;
-//Imprimiendo en consola variables
-Console.WriteLine(valor1);
-Console.WriteLine(valor2);
-
-Console.WriteLine(valor1);
-Console.WriteLine(valor2);
 
