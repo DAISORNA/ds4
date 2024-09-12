7870a864701b971529ba9d1cb3ec9838b2cb6bb9
dion <dortegana@hotmail.com>
2024-09-12 05:18:45 -0500

    Laboratorio 5 Finalizado

---
+++ Laboratorio5/Laboratorio5-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+internal class Program
+{
+    private int[] sueldos; // Declaramos un vector
+
+    public void Cargar()
+    {
+        sueldos = new int[6]; // Inicializamos el vector en 5
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("Ingrese sueldo del operario "+f+": ");
+            string linea;
+            linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea); // Asignamos los 5 sueldos al vector
+        }
+    }
+
+    // Muestra los sueldos de los operarios en el vector sueldos[f]
+    public void Imprimir()
+    {
+        Console.WriteLine("Los 5 sueldos de los operarios:");
+        for (int f = 1; f <= 5; f++) 
+        {
+            Console.Write("[" + sueldos[f] + "] ");
+        }
+        Console.ReadKey();
+    }
+
+    // Main principal
+    static void Main(string[] args)
+    {
+        Program pv = new Program(); // Cambié el nombre de la clase a Program
+        pv.Cargar();
+        pv.Imprimir();
+    }
+}
