e30770bfd62f6c0c539723342100c4ebabab4d07
dion <dortegana@hotmail.com>
2024-10-18 18:09:54 -0500

    Laboratorio14 Finalizado

---
+++ Laboratorio14/Laboratorio14/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Laboratorio14
+{
+    partial class frmProductos
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProductos));
+            toolStrip1 = new ToolStrip();
+            tsbNuevo = new ToolStripButton();
+            tsbGuardar = new ToolStripButton();
+            tsbCancelar = new ToolStripButton();
+            tsbEliminar = new ToolStripButton();
+            toolStripLabel1 = new ToolStripLabel();
+            toolStripLabel2 = new ToolStripLabel();
+            tsId = new ToolStripTextBox();
+            tsbBuscar = new ToolStripButton();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtId = new TextBox();
+            txtNombre = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            txtPrecio = new TextBox();
+            txtStock = new TextBox();
+            btnSalir = new Button();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { tsbNuevo, tsbGuardar, tsbCancelar, tsbEliminar, toolStripLabel1, toolStripLabel2, tsId, tsbBuscar });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 27);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbNuevo
+            // 
+            tsbNuevo.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbNuevo.Image = (Image)resources.GetObject("tsbNuevo.Image");
+            tsbNuevo.ImageTransparentColor = Color.Magenta;
+            tsbNuevo.Name = "tsbNuevo";
+            tsbNuevo.Size = new Size(29, 24);
+            tsbNuevo.Text = "toolStripButton1";
+            tsbNuevo.Click += tsbNuevo_Click;
+            // 
+            // tsbGuardar
+            // 
+            tsbGuardar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbGuardar.Image = (Image)resources.GetObject("tsbGuardar.Image");
+            tsbGuardar.ImageTransparentColor = Color.Magenta;
+            tsbGuardar.Name = "tsbGuardar";
+            tsbGuardar.Size = new Size(29, 24);
+            tsbGuardar.Text = "toolStripButton2";
+            tsbGuardar.Click += tsbGuardar_Click;
+            // 
+            // tsbCancelar
+            // 
+            tsbCancelar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbCancelar.Image = (Image)resources.GetObject("tsbCancelar.Image");
+            tsbCancelar.ImageTransparentColor = Color.Magenta;
+            tsbCancelar.Name = "tsbCancelar";
+            tsbCancelar.Size = new Size(29, 24);
+            tsbCancelar.Text = "toolStripButton3";
+            tsbCancelar.Click += tsbCancelar_Click;
+            // 
+            // tsbEliminar
+            // 
+            tsbEliminar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbEliminar.Image = (Image)resources.GetObject("tsbEliminar.Image");
+            tsbEliminar.ImageTransparentColor = Color.Magenta;
+            tsbEliminar.Name = "tsbEliminar";
+            tsbEliminar.Size = new Size(29, 24);
+            tsbEliminar.Text = "toolStripButton4";
+            tsbEliminar.Click += tsbEliminar_Click;
+            // 
+            // toolStripLabel1
+            // 
+            toolStripLabel1.Name = "toolStripLabel1";
+            toolStripLabel1.Size = new Size(0, 24);
+            // 
+            // toolStripLabel2
+            // 
+            toolStripLabel2.Name = "toolStripLabel2";
+            toolStripLabel2.Size = new Size(99, 24);
+            toolStripLabel2.Text = "Buscar por id:";
+            // 
+            // tsId
+            // 
+            tsId.Name = "tsId";
+            tsId.Size = new Size(100, 27);
+            // 
+            // tsbBuscar
+            // 
+            tsbBuscar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbBuscar.Image = (Image)resources.GetObject("tsbBuscar.Image");
+            tsbBuscar.ImageTransparentColor = Color.Magenta;
+            tsbBuscar.Name = "tsbBuscar";
+            tsbBuscar.Size = new Size(29, 24);
+            tsbBuscar.Text = "toolStripButton5";
+            tsbBuscar.Click += tsbBuscar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(175, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(22, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Id";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(217, 55);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Nombre";
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(12, 95);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(125, 27);
+            txtId.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(217, 95);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(382, 27);
+            txtNombre.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 167);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Precio";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(217, 167);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 20);
+            label5.TabIndex = 7;
+            label5.Text = "Stock";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(12, 214);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(125, 27);
+            txtPrecio.TabIndex = 8;
+            // 
+            // txtStock
+            // 
+            txtStock.Location = new Point(217, 214);
+            txtStock.Name = "txtStock";
+            txtStock.Size = new Size(125, 27);
+            txtStock.TabIndex = 9;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(12, 290);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(94, 29);
+            btnSalir.TabIndex = 10;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // frmProductos
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalir);
+            Controls.Add(txtStock);
+            Controls.Add(txtPrecio);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(txtNombre);
+            Controls.Add(txtId);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(toolStrip1);
+            Name = "frmProductos";
+            Text = "Form1";
+            Load += Form1_Load;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton tsbNuevo;
+        private ToolStripButton tsbGuardar;
+        private ToolStripButton tsbCancelar;
+        private ToolStripButton tsbEliminar;
+        private ToolStripLabel toolStripLabel1;
+        private ToolStripLabel toolStripLabel2;
+        private Label label1;
+        private ToolStripTextBox tsId;
+        private ToolStripButton tsbBuscar;
+        private Label label2;
+        private Label label3;
+        private TextBox txtId;
+        private TextBox txtNombre;
+        private Label label4;
+        private Label label5;
+        private TextBox txtPrecio;
+        private TextBox txtStock;
+        private Button btnSalir;
+    }
+}
